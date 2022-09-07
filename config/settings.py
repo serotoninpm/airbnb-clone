abb48ea7ba84bcdd5f2e94d846c0ed8effa62afc
serotoninpm <serotoninpm>
2022-09-07 14:37:33 +0900

    status15

--- config/settings.py
+++ config/settings.py
@@ -182,7 +182,7 @@ if not DEBUG:
     STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = "sairbnb-clone-nomadcoders-wook2"
+    AWS_STORAGE_BUCKET_NAME = "airbnb-clone-nomadcoders-wook2"
     AWS_DEFAULT_ACL = "public-read"
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com"
